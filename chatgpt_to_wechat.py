4630ef75617ba02991287eab4c019c257e6a65fb
Chunhua "Leo" Liao <chunhualiao@gmail.com>
2023-04-01 12:12:32 -0700

    cleaned up, fixed unique question detection also

--- chatgpt_to_wechat.py
+++ chatgpt_to_wechat.py
@@ -1,13 +1,17 @@
 # Origional code https://github.com/marticztn/WeChat-ChatGPT-Automation
 # modified by: C. Liao
-#  step 1. get the window
-#   step 2: taking screenshots
-#   step 3: extracting text
+
+# Algorithm
+#   step 1 get the wechat chat window by its title
+#   step 2 taking screenshots
+#   step 3 extracting text
+#   step 4 sending text to OpenAI's API
+#   step 5 sending the response back to the chat window
 import openai
 import time
 import cv2
 import pyautogui as auto
-import numpy
+import numpy as np
 import pyperclip
 from PIL import Image
 # from AppKit import NSPasteboard, NSStringPboardType
@@ -17,8 +21,8 @@ import json
 
 import pygetwindow as gw
 import sys
-import numpy as np
-import tempfile
+#import numpy as np
+#import tempfile
 #import pytesseract # OCR quality is bad for Chinese.
 #import easyocr  # OCR quality is bad for mixed Enlighs and Chinese, also slow on CPU only machine
 import logging
@@ -26,19 +30,41 @@ from paddleocr import PaddleOCR
 from dotenv import load_dotenv
 import os
 
-import time
+#  User configurable variables, must change to your own settings
+# -------------------
+# TODO: Replace this with the actual window title of the chat program
+# if not certain, run this program once to see all window titles.
+chat_window_title = 'chatgpt test'
+#QUESTION_PREFIX = '@chatgpt'
+QUESTION_PREFIX = '机器人'
+# How long in seconds to wait before checking for new messages again if no new messages are found
+SLEEP_TIME = 5    
 
-# Configure logging level for PaddleOCR
-#logging.getLogger("paddleocr").setLevel(logging.ERROR)
-# Set up the logging level
-# logging.basicConfig(level=logging.ERROR)
-# Locate the logger used by PaddleOCR
-paddle_logger = logging.getLogger('ppocr')
+# TIME limit of the chatbot: 1 hour each time
+TIME_LIMT = 3600
 
-# Set the logging level to ERROR (suppress DEBUG and WARNING messages)
-paddle_logger.setLevel(logging.ERROR)
+# TODO: best use "greenshot" to find the coordinates of the chat window's input box
+# the input message box's start position
+# using this as the anchor point!!
+inx = 1050
+iny = 1065
 
+# shift to hit middle of Copy menu item
+plusx = 41
+plusy = 14
+
+# which OpenAI model to use
+MODEL = "gpt-3.5-turbo"
+
+# Internal variables 
+# -------------------
+# screenshot of the chat window, used for OCR
 prev_image_array = np.array([],dtype=float)
+
+# global questions dictionary: avoid answering the same question twice
+# using set to be sure that the question is unique
+question_dict = set() 
+
 # load the contents of the .env file into os.environ
 load_dotenv()
 
@@ -48,38 +74,10 @@ api_key = os.environ.get('API_KEY')
 if api_key is None:
     print("OpenAI's API key not found in environment variables")
     sys.exit(1)
-    
-MODEL = "gpt-3.5-turbo"
+
 API_KEY = api_key
 IMG_NAME = '1.png'
 openai.api_key = API_KEY
-
-# global questions
-questions = {}
-
-# TODO: best use "greenshot" to find the coordinates of the chat window's input box
-# the input message box's start position
-# using this as the anchor point!!
-inx = 1035
-iny = 1062
-
-# shift to hit middle of Copy menu item
-plusx = 41
-plusy = 14
-
-# The greenshot program can be used to find the coordinates of the chat window.
-# MUST update the region coordinates for your own computer!!
-# the chat message display region's coordinates
-left = 1315
-top = 64
-width = 1851
-height = 670
-
-# where to grab the actual message content from the screenshot
-# the output message box's left bottom corner
-mx = 1401
-my = 642
-
 
 # msgs is a list of dictionaries.
 # the first dictionary is the initial message from the assistant
@@ -87,88 +85,9 @@ msgs = [
     {"role": "system", "content": "你是一个很幽默的助手"}
 ]
 
-# No longer used
-# Analyze screenshot
-# continuously take screenshots of a region on the screen and
-# analyze the color of the background and the content.
-def analyzeScreenshot():
-    # background color RBG values
-    # paint program will find out RGB values of the background color
-    # used as a reference to determine if the screen content has changed
-    prev_avg_color = numpy.array([245, 245, 245])
-    while True:
-        # x,y, width, height
-        auto.screenshot(IMG_NAME, region=(left, top, width, height))
-        img = cv2.imread(IMG_NAME)  # image file is loaded as a numpy array
-
-        # obtain the dominant color of the screenshot
-        thief = ColorThief(IMG_NAME)
-        dom_color = thief.get_color(quality=1)
-
-        avg_color_per_row = numpy.average(img, axis=0)
-        avg_color = numpy.average(avg_color_per_row, axis=0)
-        color_arr = numpy.array(img)
-        fin_arr = color_arr.tolist()
-
-        # print(dom_color)
-        # print(auto.position())
-        # check if the background color has changed
-        # RGB 20,20,20 is black, 24,24,24 is gray
-        if not (numpy.array_equal(avg_color, prev_avg_color)) and \
-                dom_color != (20, 20, 20) and \
-                dom_color != (24, 24, 24) and \
-                [24, 24, 24] not in fin_arr[99]:
-            content: str = copyMessage()
-
-            bot_prefix = '@chatgpt'
-            if content.startswith(bot_prefix):
-                response = getAnswer(content.removeprefix(bot_prefix))
-                print(response, end='\n')
-                pyperclip.copy(response)
-                sendMessage(response)
-            else:
-                print('Skipping message without matching prefix')
-                # time.sleep(0.2)  # only sleep when no request is sent
-            # pyperclip.copy(response)
-            # sendMessage(response)
-
-        prev_avg_color = avg_color
-
-
-# Copy message content from the chat window, top left corner at (a,b) position
-# all new messages will show up in the top left corner of the screen eventually.
-def copyMessage() -> str:
-    global msgs
-    # move the mouse cursor to the message box and copy message
-    # simulating the process of copying the message using the mouse
-    # ??? this is the message window's position on my computer.
-    a = mx  # 10
-    b = my  # 10
-    auto.moveTo(a, b)
-    auto.rightClick()
-    # shift to the right and down : hit middle of copy!!
-    auto.moveTo(a+plusx, b+plusy)
-    auto.click()  # we should click on the "copy" option now
-    auto.moveTo(a, b)
-
-    # time.sleep(0.2) # we don't need this may be
-
-    # pb = NSPasteboard.generalPasteboard()
-    pb = clipboard.paste()  # bug: this does not work as expected!!
-
-    # pbstring: str = pb.stringForType_(NSStringPboardType)
-    pbstring: str = pb
-    msgs.append({"role": "user", "content": pbstring})
-
-    return pbstring
-
 # move the mouse cursor to the message box and send message using pasting
 # getgreenshot.org software: printscreen to find the coordinates.
-
-
-def sendMessage(msg: str):
-    # this position is for my computer's window layout only.
-    # TODO: must update for your own computer
+def sendMessage(msg: str):    
     auto.moveTo(inx, iny)
     auto.click()
     # auto.hotkey('command', 'v')  # pyautogui
@@ -176,9 +95,7 @@ def sendMessage(msg: str):
     auto.press('enter')
 
 # Calling ChatGPT API
-# add answer to the msgs list, with a size limit of 25
-
-
+# Add answer to the msgs list, with a size limit
 def getAnswer(msg: str) -> str:
     # global variable msgs, accessible anywhere in the program
     # msgs is a list of dictionaries!
@@ -212,7 +129,7 @@ def getAnswer(msg: str) -> str:
 
     return response
 
-# the main program
+# the old main program, not used anymore
 # analyzeScreenshot()
 
 
@@ -222,9 +139,11 @@ def getAnswer(msg: str) -> str:
 #pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # capture the new questions from the chat window
+# using OCR of the screenshot to extract the text, find the new questions starting with "@chatgpt"
+
 def capture_chat_text(window_title):
     global prev_image_array
-    global questions
+    global question_dict
     result =[]      
     windows = gw.getWindowsWithTitle(window_title)
     if len(windows) <= 0:
@@ -247,8 +166,7 @@ def capture_chat_text(window_title):
         current_image_array = np.array(screenshot)
         
         if np.array_equal(prev_image_array, current_image_array):
-            print("The images are the same. No new questions.")
-            time.sleep(5) # must sleep for a while, otherwise the program will run too fast
+            print("The images are the same. No new questions.")            
             return result
         else:
             print("The images are different. Detecting new questions...")        
@@ -260,20 +178,22 @@ def capture_chat_text(window_title):
         # Perform OCR on the screenshot
         chat_text = ocr.ocr( current_image_array)
 
-
         for line in chat_text[0]:
                 line= line[-1][0]
                 # check if the line contains a prefix string of "@chatgpt"
-                if line.startswith('@chatgpt'):
+                if line.startswith(QUESTION_PREFIX):
                     # get the message
-                    question = line.split('@chatgpt')[1]   
+                    question = line.split(QUESTION_PREFIX)[1]   
                     # trim the leading and trailing spaces
                     question = question.strip()   
-                    if question not in questions:
-                        questions[question] = 1
+                    if question not in question_dict:
+                        question_dict.add(question)
+                        print(f"New question: '{question}' has been found. Dict size is {len(question_dict)} now.")
                         # print(question)                                                    
                         # add the new question into a list of results
                         result.append(question)
+                    else:
+                        print(f"The question: '{question}' has been asked before. Ignoring it.")
                         
         return result                                                       
 # Each inner list contains information about the detected text line,
@@ -317,24 +237,29 @@ def capture_chat_text(window_title):
 
 if __name__ == '__main__':
     
-    start_time = time.time()
-    
+    start_time = time.time()    
     if sys.version_info >= (3, 11):
-        sys.exit("This script only supports Python versions earlier than 3.11, due to the OSCR package used.")
+        sys.exit("This script only supports Python versions earlier than 3.11, due to the OCR package used.")
 
-    # Get all visible windows
+    # Get all visible windows, print the title of each window
     windows = gw.getAllWindows()
-
-    # Print the title of each window
     print('---------------------------------')
     print("All visible windows' titles are:")
     for window in windows:
         if window.visible:            
             print(window.title)
-    
-    # Replace this with the actual window title of the chat program
-    chat_window_title = 'chatgpt test'
 
+    # Configure logging level for PaddleOCR
+    # turn off all warnings
+    
+    #logging.getLogger("paddleocr").setLevel(logging.ERROR)
+    # Set up the logging level
+    # logging.basicConfig(level=logging.ERROR)
+    # Locate the logger used by PaddleOCR
+    paddle_logger = logging.getLogger('ppocr')
+    # Set the logging level to ERROR (suppress DEBUG and WARNING messages)
+    paddle_logger.setLevel(logging.ERROR)
+    
     while True:
         new_questions = capture_chat_text(chat_window_title)
         if new_questions:
@@ -347,13 +272,12 @@ if __name__ == '__main__':
                 # paste the answer to the chat window
                 sendMessage(answer)            
         else:
-            print("No new questions extracted from the Chat window.")
-            #time.sleep(1)  # Adjust the monitoring interval as needed
+            print(f"No new questions extracted from the Chat window titled '{chat_window_title}', waiting for {SLEEP_TIME} seconds before next try...")
+            time.sleep(SLEEP_TIME) # must sleep for a while, otherwise the program will run too fast
         elapsed_time = time.time() - start_time
-        print(f">>>>>>>  Elapsed time: {elapsed_time:.2f} seconds")
+        print(f">>>>>>>  Elapsed time: {elapsed_time:.2f} of total limit of {TIME_LIMT:.2f} seconds")
         # if 
-        if elapsed_time > 3600:
+        if elapsed_time > TIME_LIMT:
             print(f">>>>>>>  Elapsed time exceeding limit, exiting...")
             break
-
-            
+         
